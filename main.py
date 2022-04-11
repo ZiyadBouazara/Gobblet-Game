89438998d120937ab9f8a265bbe2e90a2ae65887
ZiyadBouazara <ziyad.bouazara1@ulaval.ca>
2022-04-10 21:44:19 -0400

    Main.py implémentation de la boucle de jeux

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 Ce programme permet de joueur au jeu Gobblet.
 """
-from api import débuter_partie, lister_parties
+from api import débuter_partie, lister_parties, jouer_coup
 from gobblet import (
     formater_jeu,
     formater_les_parties,
@@ -13,7 +13,7 @@ from joueur import Joueur
 from plateau import Plateau
 
 # Mettre ici votre secret récupérer depuis le site de PAX
-SECRET = ""
+SECRET = "ed0f6548-6830-4e2a-9ffb-4c6d94e5b28b"
 
 
 if __name__ == "__main__":
@@ -25,4 +25,10 @@ if __name__ == "__main__":
         id_partie, plateau, joueurs = débuter_partie(args.IDUL, SECRET)
         while True:
             # Implémentez votre boucle de jeu
-            pass
+            joueur1 = Joueur(joueurs[0].get('nom'), 1, joueurs[0].get('piles'))
+            joueur2 = Joueur(joueurs[1].get('nom'), 2, joueurs[1].get('piles'))
+            plateau_de_jeux = Plateau(plateau)
+            print(formater_jeu(plateau_de_jeux, [joueur1, joueur2]))
+            origine, destination = joueur1.récupérer_le_coup(plateau_de_jeux)
+            id_partie, plateau, joueurs = jouer_coup(
+                id_partie, origine, destination, args.IDUL, SECRET)
